14ec3b02c00d7c68d45a43a30969349578cdf74a
Oksana1992-hash <nox23061992@gmail.com>
2026-05-16 14:35:49 +0300

    changed build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-set -e  # Остановить на первой ошибке
-
-# 1. Скачиваем uv и активируем окружение
+#!/usr/bin/env bash
+# 1. скачиваем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
 # 2. Устанавливаем зависимости
-uv pip install -r requirements.txt
+pip install -r requirements.txt
 
 # 3. Собираем статику
 python manage.py collectstatic --noinput
